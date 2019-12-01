1bd0245d8e350efc663ccd02a7e318c7cc33213f
Lasse Meland <sjark89@gmail.com>
2019-12-01 11:47:40 +0100

    Dag 01

---
+++ Dag01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Dag01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sheeps = File.ReadAllText("Input\\sau.txt").Split(',')
+                .Select(a => int.Parse(a.Trim()))
+                .ToList();
+
+            var dragonSize = 50;
+            var day = 0;
+            var extraSheeps = 0;
+            var daysInRowWithoutFood = 0;
+
+            while (daysInRowWithoutFood != 5)
+            {
+                var sheepsAvailable = sheeps[day] + extraSheeps;
+                extraSheeps = sheepsAvailable - dragonSize;
+
+                if (extraSheeps >= 0)
+                {
+                    dragonSize += 1;
+                    daysInRowWithoutFood = 0;
+                }
+                else
+                {
+                    dragonSize -= 1;
+                    daysInRowWithoutFood++;
+                    extraSheeps = 0;
+
+                    if (daysInRowWithoutFood == 5)
+                    {
+                        break;
+                    }
+                }
+
+                day++;
+            }
+
+            Console.WriteLine($"Day: {day}");
+        }
+    }
+}
