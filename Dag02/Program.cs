f852c63d9989ed0cc8ae91db6a4b226381553812
Lasse Meland <sjark89@gmail.com>
2019-12-02 17:20:19 +0100

    Dag 02

---
+++ Dag02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Dag02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var world = File.ReadAllLines("Input\\world.txt");
+            var water = 0;
+
+            foreach (var line in world)
+            {
+                var firstRock = false;
+                var air = 0;
+
+                foreach (var spot in line)
+                {
+                    if (firstRock && spot == ' ')
+                    {
+                        air += 1;
+                    }
+                    else if (spot == '#')
+                    {
+                        firstRock = true;
+                        water += air;
+                        air = 0;
+                    }
+                }
+            }
+
+            Console.WriteLine(water);
+        }
+    }
+}
