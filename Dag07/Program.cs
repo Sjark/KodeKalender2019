b48783eda00c7831c42a84b3b3283dd0b02e722d
Lasse Meland <sjark89@gmail.com>
2019-12-07 11:50:05 +0100

    Dag 07

---
+++ Dag07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Dag07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long x = 7;
+
+            for (long y = 2; y <= 27644436; y++)
+            {
+                long b = y * x;
+                long r = b % 27644437;
+
+                if (r == 1)
+                {
+                    long z = 5897 * y;
+                    long code = z % 27644437;
+
+                    Console.WriteLine($"Day: {x} code: {code}");
+                    break;
+                }
+            }
+        }
+    }
+}
