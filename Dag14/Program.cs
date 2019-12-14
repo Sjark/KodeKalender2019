e35324338835f9b1eca3ed07f27d006b26b9dfba
Lasse Meland <sjark89@gmail.com>
2019-12-14 23:58:12 +0100

    Dag 14 optimized

--- Dag14/Program.cs
+++ Dag14/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Dag14
@@ -8,42 +9,57 @@ namespace Dag14
     {
         static void Main(string[] args)
         {
-            var alphabet = new List<int>
+            var stopwatch = Stopwatch.StartNew();
+            var alphabet = new byte[]
             {
                 2, 3, 5, 7, 11
             };
 
             var sequenceLength = 217532235;
-            var sequence = new int[217532235];
+            var sequence = new byte[sequenceLength];
 
             for (int y = 0; y < alphabet[0]; y++)
             {
                 sequence[y] = alphabet[0];
             }
 
-            var i = 1;
-            var pos = 1;
+            int i = 1;
+            int pos = 1;
+            int sumOf7 = 0;
+            byte curChar = 1;
 
             while (pos < sequenceLength)
             {
-                var currentCharacter = alphabet[i % alphabet.Count];
+                var currentCharacter = alphabet[curChar];
 
-                for (int y = 0; y < sequence[i]; y++)
+                if (curChar == 4)
+                {
+                    curChar = 0;
+                }
+                else
+                {
+                    curChar += 1;
+                }
+
+
+                for (int y = 0; y < sequence[i] && pos < sequenceLength; y++)
                 {
                     sequence[pos] = currentCharacter;
 
-                    pos += 1;
-
-                    if (pos == sequenceLength)
+                    if (currentCharacter == 7)
                     {
-                        break;
+                        sumOf7 += 7;
                     }
+
+                    pos += 1;
                 }
 
                 i++;
             }
 
-            Console.WriteLine(sequence.Where(a => a == 7).Sum());
+            stopwatch.Stop();
+            Console.WriteLine(sumOf7);
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
         }
     }
 }
