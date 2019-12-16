d7d0873870678afe5b9cefda406b6d337289ae7e
Lasse Meland <lasse.meland@cegal.com>
2019-12-16 09:23:05 +0100

    Dag 16

---
+++ Dag16/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace Dag16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fjord = File.ReadAllLines("Input\\fjord.txt");
+
+            var goingNorth = true;
+            var bloc = (x: -1, y: -1);
+            var crossings = 1;
+
+            for (int y = 0; y < fjord.Length; y++)
+            {
+                for (int x = 0; x < fjord[y].Length; x++)
+                {
+                    if (fjord[y][x] == 'B')
+                    {
+                        bloc.x = x;
+                        bloc.y = y;
+
+                        break;
+                    }
+                }
+
+                if (bloc.x != -1)
+                {
+                    break;
+                }
+            }
+
+            while (bloc.x != fjord[0].Length)
+            {
+                if (goingNorth && bloc.y - 3 > 0 && fjord[bloc.y - 3][bloc.x] != '#')
+                {
+                    bloc.x += 1;
+                    bloc.y -= 1;
+                }
+                else if (!goingNorth && bloc.y + 3 < fjord.Length - 1 && fjord[bloc.y + 3][bloc.x] != '#')
+                {
+                    bloc.x += 1;
+                    bloc.y += 1;
+                }
+                else
+                {
+                    crossings += 1;
+                    goingNorth = !goingNorth;
+                    bloc.x += 1;
+                }
+            }
+
+            Console.WriteLine(crossings);
+        }
+    }
+}
---
+++ Dag16/readme.md
@@ -0,0 +1,58 @@
+﻿# Seilkryss
+
+Av: Didrik Pemmer Aalen
+
+Birte befinner seg på en seilbåt helt innerst i den lange julefjorden. Hun har et ønske om å komme seg ut, slik at hun kommer seg hjem til jul, men vindforholdene gjør det vanskelig. For å komme seg ut av fjorden må hun følge disse reglene:
+
+1. Hun kan kun krysse nordøst (x+1, y+1) og sørøst (x+1, y-1).
+2. For å krysse færrest mulige ganger snur hun når hun er 20 meter fra land (nord/sør).
+
+## Oppgave
+Gitt [følgende fjord](https://julekalender.knowit.no/resources/2019-luke16/fjord.txt), hvor mange ganger ender Birte opp med å krysse for å komme seg ut av fjorden når hun begynner å krysse mot nordøst?
+
+## Eksempel
+
+Her er følgende definert:
+
+```
+B = Birte
+# = Land
+Tomme ruter er vann
+/ og \ viser ruten til Birte i svaret
+```
+
+Hver rute er 10m<sup>2</sup>
+
+Gitt følgende fjord:
+
+```
+#####################
+#    ###
+#            
+#
+#
+#
+#
+#
+#B
+###    ####     #   #
+#####################
+```
+
+Blir kryssingene slik:
+
+```
+#####################
+#    ###
+#            
+#           /\      /
+#    /\    /  \    /
+#   /  \  /    \  /
+#  /    \/      \/
+# /              
+#B
+###    ####     #   #
+#####################
+```
+
+Og Birte ender med 5 kryssinger.
