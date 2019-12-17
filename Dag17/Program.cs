30dac8f0e5fb04fd9e940c0ff578f0a960c7de6b
Lasse Meland <sjark89@gmail.com>
2019-12-17 08:57:46 +0100

    Day 17, generate triangular numbers

---
+++ Dag17/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Dag17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = Enumerable.Range(0, 1000000)
+                .Select(a => a * (a + 1) / 2)
+                .ToArray();
+
+            Console.WriteLine(range[5]);
+        }
+    }
+}
