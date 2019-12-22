04b022f9772216ed281f602ceeadc67d64116604
Lasse Meland <sjark89@gmail.com>
2019-12-22 10:49:04 +0100

    Dag 22

---
+++ Dag22/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Dag22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trees = File.ReadAllLines("Input\\forest.txt");
+
+            var treeIndexes = trees[^1].Select((a, i) => a == '#' ? i : -1).Where(a => a != -1).ToArray();
+
+            var totalHeight = 0;
+
+            foreach (var index in treeIndexes)
+            {
+                var currentPart = 1;
+                var treeHeight = 0;
+
+                while (true && currentPart <= trees.Length)
+                {
+                    var currentTreePart = trees[^currentPart][index];
+
+                    if (currentTreePart == '#')
+                    {
+                        treeHeight += 20;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    currentPart += 1;
+                }
+
+                totalHeight += treeHeight;
+            }
+
+            Console.WriteLine(totalHeight / 100.0 * 200);
+        }
+    }
+}
